1e0f95bfb51113526660694e66946dafafbdb455
Sam Partee <Partees21@gmail.com>
2025-03-10 13:59:55 -0700

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+    logo: <span>Open Tool Calling</span>,
+    project: {
+        link: "https://github.com/opentoolcalling/specification",
+    },
+    chat: {
+        link: "https://discord.com",
+    },
+    docsRepositoryBase: "https://github.com/opentoolcalling/docs",
+    footer: {
+        text: "Open Tool Calling",
+    },
+};
 
-export default config
+export default config;
