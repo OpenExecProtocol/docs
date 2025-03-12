90589e9c08de3ddf65cffe2d8905cfc1a4c7c276
Nate Barbettini <nate@arcade.dev>
2025-03-12 12:15:09 -0700

    Cleanup and fix OG text (#3)

--- theme.config.tsx
+++ theme.config.tsx
@@ -6,6 +6,20 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/OpenToolCalling/Specification',
   },
   docsRepositoryBase: 'https://github.com/OpenToolCalling/docs',
+  head: () => {
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Open Tool Calling" />
+        <meta name="og:title" content="Open Tool Calling" />
+        <meta
+          name="og:description"
+          content="The Open Tool Calling specification"
+        />
+        <meta name="og:url" content="https://opentoolcalling.org" />
+      </>
+    );
+  },
   footer: {
     text: 'Open Tool Calling',
   },
