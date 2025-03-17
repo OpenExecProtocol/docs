23b0c4432bdb967214c4305a2038f8ad81f43b16
Sergio Serrano <sdserranog@gmail.com>
2025-03-17 00:23:09 -0300

    Upgrade to Nextra 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
-import nextra from 'nextra';
 import { remarkMermaid } from '@theguild/remark-mermaid';
+import nextra from 'nextra';
 
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
   mdxOptions: {
     remarkPlugins: [remarkMermaid],
+    rehypePrettyCodeOptions: {
+      theme: 'everforest-dark',
+    },
   },
 });
 
